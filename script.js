8a07c45c1b6fbf3b4210976f9e66e747de4a9882
Thalha <76985723+thalha-dev@users.noreply.github.com>
2023-02-06 21:18:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your code here
+let input = document.querySelector("#evaluatedText");
+let output = document.querySelector("#ltterCount");
+
+function handleInputChange() {
+    let value = input.value;
+    let count = value.length;
+    output.innerHTML = count;
+}
